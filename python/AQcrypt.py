91dda82be65bd2466e2346aa64b7a8beb747c611
SM1SHi <sergejpro126@gmail.com>
2023-02-14 18:20:38 +0100

    added filterSymbols(), no ValueError when string contains special characters

--- python/AQcrypt.py
+++ python/AQcrypt.py
@@ -2,16 +2,22 @@
 # converts lowercase to uppercase 
 alph = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+#function to ignore symbols, can only be used in stringToList()
+def filterSymbols(character):
+    filteredChar = ''.join(filter(str.isalnum, character))
+    return filteredChar
+
 # checks character index of given string, converts it to lowercase
 def charsAQ(charstoConv):
     lowerChars = charstoConv.lower()
-    convertedStr = alph.index(lowerChars)
+    convertedStr = alph.index(lowerChars)        
     return convertedStr
 
 #convets a string to list using literal magic
 def stringToList(string):
     list1 = []
-    list1[:0] = string.replace(" ", "")
+    filStr = filterSymbols(string)
+    list1[:0] = filStr.replace(" ", "")
     return list1
 
 # map charsAQ to all elements in list1 
